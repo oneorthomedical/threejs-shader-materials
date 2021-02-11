86ed582ecb30fecab7508fb66e75d5840ee6c22d
HOUSSAM KARRACH <karrach.houssam@gmail.com>
2021-02-11 11:48:45 +0100

    Update ShaderPhongMaterial.d.ts

--- lib/ShaderPhongMaterial.d.ts
+++ lib/ShaderPhongMaterial.d.ts
@@ -2,7 +2,7 @@ import { Color, ShaderMaterial, ShaderMaterialParameters, Texture } from "three"
 import { IMap } from "./chunk/MapChunk";
 /**
  * MeshPhongMaterialに準じるShaderMaterialクラス。
- *
+ * @Houssk update
  * @see https://github.com/mrdoob/three.js/blob/76c64b23d422dcfb36a28353f45b1effa1f68c5a/src/renderers/shaders/ShaderLib.js#L53
  */
 export declare abstract class ShaderPhongMaterial extends ShaderMaterial implements IMap {
@@ -46,24 +46,12 @@ export declare abstract class ShaderPhongMaterial extends ShaderMaterial impleme
      * @deprecated Use uniformOpacity, To be removed in version 0.3.0
      * @see https://github.com/microsoft/TypeScript/pull/37894
      */
-    get opacity(): number;
-    /**
-     * 透明度
-     */
     get uniformOpacity(): number;
     /**
      * 透明度
      * @param value
      * @deprecated Use uniformOpacity, To be removed in version 0.3.0
      * @see https://github.com/microsoft/TypeScript/pull/37894
-     */
-    set opacity(value: number);
-    /**
-     * 透明度
-     * opacityは基底クラスのMaterialのコンストラクタ内で明示的に1.0が代入される。
-     * この段階でuniformsはundefinedなので、そのままでは初期化できない。
-     * このsetterでは受け取った値をprivate変数に保存して、初期化後にuniformsに再代入する。
-     * @param value
      */
     set uniformOpacity(value: number);
     protected _opacity: number;
